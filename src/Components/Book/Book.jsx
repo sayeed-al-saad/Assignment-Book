0fd529cb449d45d3e59ef38028ec4234bd82e329
sayeed-al-saad <sayeedahmed15162@gmail.com>
2024-12-02 16:58:41 +0600

    6th

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,11 +1,23 @@
+import PropTypes from 'prop-types';
 
 
-const Book = () => {
+const Book = ({book}) => {
+    
+    const {bookId, bookName, author, image, review, totalPages, rating, category, publisher, yearOfPublishing} = book;
+
     return (
         <div>
-            
+            <img src="url" alt="" />
+            <h3>Hi {bookId}</h3>
         </div>
     );
 };
+
+Book.propTypes = {
+    book: PropTypes.shape({
+        bookId: PropTypes.number.isRequired, // Assuming bookId is a number
+    }).isRequired
+};
+
 
 export default Book;
--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Book from "../Book/Book";
 
 
 const Books = () => {
+
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch('books.json')
+        .then(res => res.json())
+        .then(data => setBooks(data))
+    }, [])
+
     return (
-        <div>
-            <h2>Books</h2>
+        <div className="py-5">
+            <h2 className="text-center text-4xl font-bold ">Books {books.length}</h2>
             <div>
-                
+                {
+                    books.map(book => <Book key={book.bookId} book ={book} ></Book>)
+                }
             </div>
         </div>
     );
